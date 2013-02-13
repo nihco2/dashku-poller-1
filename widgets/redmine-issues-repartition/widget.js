6b95fd64b361a810e9c38b3cca3d327a57ce0fc6
Sylvain Faucherand <syl.faucherand@gmail.com>
2013-02-13 18:53:37 +0100

    visual effect when update occurs

--- widgets/redmine-issues-repartition/widget.js
+++ widgets/redmine-issues-repartition/widget.js
@@ -2,7 +2,7 @@
 // check out the Docs section titled 'Using 3rd-party JS libraries'.
 
 // The widget's html as a jQuery object
-var widget = this.widget, r = null;
+var widget = this.widget;
 
 // This runs when the widget receives a transmission
 this.on('load', function(data){
@@ -15,38 +15,56 @@ this.on('load', function(data){
 
 this.on('transmission', function(data){
   
-  if(r) r.remove();
+  var pieholder = widget.find('#pieholder');
   
-  widget.find('#pieholder').html();
-  
-  r = Raphael("pieholder");
-  
-  var
-      values = data.values,
-      names = data.names,
-      pie = r.piechart(100, 100, 80, values, {
-        legend: names,
-        legendpos: "east",
-        legendcolor: "white"
-      })
- ;
+ 	pieholder.fadeOut(function(){
     
- pie.hover(function () {
-    this.sector.stop();
-    this.sector.scale(1.1, 1.1, this.cx, this.cy);
+        // hiding + filling out container and switch to visibility off for canvas rendering
+        pieholder.html();
+        pieholder
+            .show()
+            .css({'visibility' : 'hidden'})
+        ;
+      
+        var r = Raphael("pieholder");
+        
+        var 
+            values = data.values, 
+            names = data.names
+        ;
+        
+        var
+            pie = r.piechart(100, 100, 80, values, {
+              legend: names,
+              legendpos: "east",
+              legendcolor: "white"
+            })
+       ;
+          
+       pie.hover(function () {
+          this.sector.stop();
+          this.sector.scale(1.1, 1.1, this.cx, this.cy);
+          
+          if (this.label) {
+            this.label[0].stop();
+            this.label[0].attr({ r: 7.5 });
+            this.label[1].attr({ "font-weight": 800 });
+          }
+        }, function () {
+          this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+          
+          if (this.label) {
+            this.label[0].animate({ r: 5 }, 500, "bounce");
+            this.label[1].attr({ "font-weight": 400 });
+          }
+        });
+        
+        // hide pieholder before fading in
+        pieholder
+            .css({'visibility' : 'visible', 'display' : 'none'})
+            .fadeIn()
+        ;
     
-    if (this.label) {
-      this.label[0].stop();
-      this.label[0].attr({ r: 7.5 });
-      this.label[1].attr({ "font-weight": 800 });
-    }
-  }, function () {
-    this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
-    
-    if (this.label) {
-      this.label[0].animate({ r: 5 }, 500, "bounce");
-      this.label[1].attr({ "font-weight": 400 });
-    }
-  });
+    });
 
 });
