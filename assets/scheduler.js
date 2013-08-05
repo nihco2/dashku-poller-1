d121c0cc9eb35bcd902522d35747e960abed37f8
Sylvain Faucherand <syl.faucherand@gmail.com>
2013-08-05 12:23:23 +0200

    adding possibility to give a delay before job starts, adding ' after 2 seconds' to the period in conf

--- assets/scheduler.js
+++ assets/scheduler.js
@@ -10,20 +10,26 @@
     
         return {
         
-            add : function(periodString, job, reference){
+            add : function(periodInstructionsString, job, reference){
+            
+                var periodInstructions = periodInstructionsString.split(' after ');
             
                 var 
-                    period = periodString.split(' '),
-                    periodValue = +period[0],
-                    periodUnit = period[1]
+                    period = periodInstructions[0].split(' '),
+                    delay = periodInstructions[1] ? periodInstructions[1].split(' ') : undefined
                 ;
+                
+                var 
+                    normPeriod = moment.duration(+period[0], period[1]).asMilliseconds(),
+                    normDelay  = (delay) ? Date.now() + moment.duration(+delay[0], delay[1]).asMilliseconds() : Date.now()
+                ;
+                
+                console.log(normPeriod, normDelay);
             
-                schedule.scheduleJob(
-                    {
-                        start : Date.now(),
-                        period : moment.duration(periodValue, periodUnit).asMilliseconds()
-                    },
-                    function(){
+                schedule.scheduleJob({
+                        start : normDelay,
+                        period : normPeriod
+                    }, function(){
                         job().then(function(result){
                         
                             var transmit = (new Transmission(reference)).addBodyParams(result);
