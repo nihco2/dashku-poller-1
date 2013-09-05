4e11313fa1efc6b9fab66803b7c5fb5a0673daea
Sylvain Faucherand <syl.faucherand@gmail.com>
2013-09-05 17:32:31 +0200

    adding dashboard and widget deployment via node deploy/ + updating mock server

--- deploy/index.js
+++ deploy/index.js
@@ -1,26 +1,71 @@
+var url         = require('url')
+var path        = require('path')
+var fs          = require('fs')
 var request     = require('request')
+var _           = require('underscore')
 var conf        = require('../conf')
-var url        = require('url')
 
 
 var apiKey      = conf.dashku.apiKey
 var apiUrl      = conf.dashku.apiUrl
 var dashName    = conf.dashku.name
+var cwd         = process.cwd()
 
 var dashId
 
 // Create dashboard
 request.post(
     url.resolve(apiUrl, 'dashboards') + '?apiKey=' + apiKey,
-    { form: { name:dashName } }, 
+    { form : { name:dashName } }, 
     createWidgets
 )
 
 function createWidgets(err, resp){
     if (err) {
-        throw new error('unable to create dashboard', err)
+        console.error('unable to create dashboard\n', err)
+        throw new error(err)
     } else {
         dashId = JSON.parse(resp.body)._id
-        console.log('got dash ID : ', dashId)
+        console.log('dashboard created with id :', dashId)
+        _.each(conf.dashku.widgets, createSingleWidget)
     }
+}
+
+function createSingleWidget(widgetSpec, name){
+    
+    if(!widgetSpec.active) return false
+    
+    var shipping = {
+        name : name,
+        css : css = fs.readFileSync(
+            path.join(cwd, widgetSpec.dir, '/client/widget.css'), 
+            {encoding : 'utf8'}
+        ),
+        js : fs.readFileSync(
+            path.join(cwd, widgetSpec.dir, '/client/widget.js'), 
+            {encoding : 'utf8'}
+        ),
+        html : fs.readFileSync(
+            path.join(cwd, widgetSpec.dir, '/client/widget.html'), 
+            {encoding : 'utf8'}
+        ),
+        width : widgetSpec.width,
+        height : widgetSpec.height
+    }
+    
+    //console.log(shipping)
+    
+    request.post(
+        url.resolve(apiUrl, 'dashboards/' + dashId + '/widgets') + '?apiKey=' + apiKey,
+        { form : shipping },
+        function(err, resp){
+            if (err) {
+                console.error('unable to create widget :', shipping.name, '\n', err)
+                throw new error(err)
+            } else {
+                 console.log('widget created :', shipping.name)
+            }
+        }
+    )
+    
 }
--- mock/index.js
+++ mock/index.js
@@ -18,11 +18,12 @@ var app = connect()
             console.log(req.body);
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify({ status: 'success' }, null, 2));
-        });
+        })
         
         router.post('/api/dashboards', function (req, res, next) {
-            console.log(req.body);
-            res.setHeader('Content-Type', 'application/json');
+            console.log('created dashboard :', req.body.name)
+            //console.log('\n-------------------------------\n', req.body)
+            res.setHeader('Content-Type', 'application/json')
             res.end(JSON.stringify({
               "name"        : "Account Management",
               "userId"      : "4fd1f55b7e9b8705a1000053",
@@ -32,8 +33,29 @@ var app = connect()
               "screenWidth" : "fixed",
               "updatedAt"   : "2012-06-08T13:51:54.972Z",
               "createdAt"   : "2012-06-08T13:51:54.972Z"
-            }, null, 2));
-        });
+            }, null, 2))
+        })
+        
+        router.post('/api/dashboards/:dashboard_id/widgets', function(req, res, next){
+            console.log('created widget :', req.body.name, 'on dash', req.params.dashboard_id)
+            //console.log('\n-------------------------------\n',req.body )
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify({
+                "userId"      : "4fd1f55b7e9b8705a1000053",
+                "_id"         : "4fd328d98caaf987a7000045",
+                "updatedAt"   : "2012-06-09T10:43:37.292Z",
+                "createdAt"   : "2012-06-09T10:43:37.292Z",
+                "height"      : 180,
+                "width"       : 200,
+                "json"        : "{\n  \"revenue\": \"346729.00\",\n  \"_id\": \"4fd328d98caaf987a7000045\"\n}",
+                "scriptType"  : "javascript",
+                "script"      : "var widget = this.widget;\n\nthis.on('load', function(data){\n  // Nothing to do\n});\n\nthis.on('transmission', function(data){\n  var salesNumber = widget.find('#salesNumber');\n  salesNumber.text('$'+data.revenue).hide().fadeIn();\n});",
+                "scopedCSS"   : ".widget[data-id='4fd328d98caaf987a7000045'] #salesNumber {\n  font-weight: bold;\n  font-size: 24pt;\n}",
+                "css"         : "#salesNumber {\n  font-weight: bold;\n  font-size: 24pt;\n}",
+                "html"        : "<div id=\"salesNumber\"></div>",
+                "name"        : "New Account Sales"
+            }, null, 2))
+        })
     
     }))
     .listen(conf.mock.port)
